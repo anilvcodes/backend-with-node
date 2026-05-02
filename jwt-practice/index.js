4f338b6d928c62613b582b551b80e05882d5d10b
Dev-Aniweb <anilkumarve51@gmail.com>
2026-05-03 00:01:28 +0530

    jwt-web-token

--- jwt-practice/index.js
+++ jwt-practice/index.js
@@ -1,11 +1,48 @@
-const  jwt = require("jsonwebtoken");
+// 
+const jwt = require('jsonwebtoken');
+const jwtPassword = "secret";
+const zod = require("zod");
 
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
 
+function signJwt(username, password) {
+    const usernameResponse = emailSchema.safeParse(username);
+    const passwordResponse = passwordSchema.safeParse(password);
+    if (!usernameResponse.success || !passwordResponse.success) {
+        return null;
+    }
 
-const value  ={
-   name :"anil k verma",
-   id :123456
+    const signature = jwt.sign({
+        username
+    }, jwtPassword);
 
+    return signature;
 }
-const token= jwt.sign(value,"secret" )
-console.log(token);
+
+function verifyJwt(token) {
+    let ans = true;
+    try {
+       jwt.verify(token, jwtPassword);
+    } catch(e) {
+       ans = false;
+    }
+    return ans;
+}
+
+function decodeJwt(token) {
+    // true, false
+    const decoded = jwt.decode(token);
+    if (decoded) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = {
+    signJwt,
+    verifyJwt,
+    decodeJwt,
+    jwtPassword
+}
