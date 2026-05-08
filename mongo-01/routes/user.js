b54cf176133894d4f311d6c85eacd5aa307eaafa
Dev-Aniweb <anilkumarve51@gmail.com>
2026-05-08 18:35:39 +0530

    newupdate

--- mongo-01/routes/user.js
+++ mongo-01/routes/user.js
@@ -17,3 +17,50 @@ router.post('/signup', (req, res) => {
         message: "User created successfully"
     })
 });
+
+router.get('/courses', async (req, res) => {
+    // Implement listing all courses logic
+     // Implement fetching all courses logic
+     const response = await Course.find({});
+
+     res.json({
+         courses: response
+     })
+});
+
+router.post('/courses/:courseId', userMiddleware, async(req, res) => {
+    // Implement course purchase logic
+    const courseId = req.params.courseId;
+    const username = req.headers.username;
+
+    await User.updateOne({
+        username: username
+    }, {
+        "$push": {
+            purchasedCourses: courseId
+        }
+    })
+    res.json({
+        message: "Purchase complete!"
+    })
+});
+
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
+    // Implement fetching purchased courses logic
+    const user = await User.findOne({
+        username: req.headers.username
+    });
+
+    console.log(user.purchasedCourses);
+    const courses = await Course.find({
+        _id: {
+            "$in": user.purchasedCourses
+        }
+    });
+
+    res.json({
+        courses: courses
+    })
+});
+
+module.exports = router
